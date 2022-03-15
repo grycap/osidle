c7e7a2b3de34cf98339909c4223e14136d35b9f8
Carlos A <caralla@upv.es>
2022-03-15 09:57:07 +0100

    add methods to delete registries, vaccumm db and process registries

--- osidle/storage.py
+++ osidle/storage.py
@@ -22,7 +22,8 @@ DEFAULT_FILENAME = "monitoring.sqlite3"
 
 def _sql(cursor, query, params = ()):
     p_debugv("{}: {}".format(query,params))
-    return cursor.execute(query, params)
+    result = cursor.execute(query, params)
+    return result
 
 class Storage:
     def __init__(self, filename = None):
@@ -127,8 +128,7 @@ class Storage:
             return []
 
         cursor = self._conn.cursor()
-        cursor.execute("select vmid from vmmonitor group by vmid")
-
+        # cursor.execute("select vmid from vmmonitor group by vmid")
 
         if (fromDate is None) and (toDate is None):
             _sql(cursor, "select vmid from vmmonitor group by vmid")
@@ -144,4 +144,77 @@ class Storage:
             toDate = toDate.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
             _sql(cursor, "select vmid from vmmonitor group by vmid where t >= ? and t <= ?", (fromDate, toDate))
 
-        return [ x for (x,) in cursor.fetchall() ]
+        return [ x for (x,) in cursor ]
+
+    def delete(self, keepFromDate, keepToDate):
+        if not self.isConnected():
+            return False
+
+        cursor = self._conn.cursor()
+
+        if (keepFromDate is None) and (keepToDate is None):
+            raise Exception("refusing to wipe the whole database")
+        elif (keepFromDate is None):
+            keepToDate = keepToDate.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+            _sql(cursor, "delete from vmmonitor where t > ?", (keepToDate,))
+        elif (keepToDate is None):
+            keepFromDate = keepFromDate.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+            _sql(cursor, "delete from vmmonitor where t < ?", (keepFromDate,))
+        else:
+            keepFromDate = keepFromDate.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+            keepToDate = keepToDate.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+            _sql(cursor, "delete from vmmonitor where t < ? or t > ?", (keepFromDate, keepToDate))
+
+        self._conn.commit()
+        return cursor.rowcount
+
+    def vaccuum(self):
+        if not self.isConnected():
+            return False
+
+        cursor = self._conn.cursor()
+        _sql(cursor, "vacuum")
+        self._conn.commit()
+        return True
+
+    def filterdata(self, filter_fnc, pre_fnc = None, post_fnc = None):
+        if not self.isConnected():
+            return False
+
+        cursor1 = self._conn.cursor()
+
+        _sql(cursor1, "select count(*) from vmmonitor")
+        count = cursor1.fetchone()[0]
+
+        if (callable(pre_fnc)):
+            pre_fnc(count)
+
+        cursor2 = self._conn.cursor()
+        _sql(cursor1, "select id, vmid, t, data from vmmonitor")
+        for (id, vmid, t, data) in cursor1:
+            data = json.loads(data)
+            data = filter_fnc(vmid, t, data)
+            if data is not None:
+                cursor2.execute("update vmmonitor set data = ? where id = ?", (json.dumps(data), id))
+
+        self._conn.commit()
+
+        if (callable(post_fnc)):
+            post_fnc()
+
+        return True
+
+def remove_unneeded_data(data):
+    if 'conflictingRequest' in data:
+        return data
+        
+    return {
+        "cpu_details": [ { "time": x["time"] } for x in data["cpu_details"] ],
+        "disk_details": [ { "read_bytes": x["read_bytes"],  "write_bytes": x["write_bytes"]} for x in data["disk_details"] ],
+        "nic_details": [ { "mac_address": x["mac_address"], "rx_octets": x["rx_octets"], "tx_octets": x["tx_octets"] } for x in data["nic_details"] ],
+        "num_cpus": data["num_cpus"],
+        "num_disks": data["num_disks"],
+        "num_nics": data["num_nics"],
+        "uptime": data["uptime"],
+        "state": data["state"],
+    }
